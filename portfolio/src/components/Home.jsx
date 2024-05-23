1d5319a4c7a1ad388f7b86997c98e4afe4e74d28
musky <mohamedmusky123@gmail.com>
2024-05-23 19:12:13 +0530

    changes

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold">
-            I'm an IT Undergraduate
+            I'm an Computer Science Undergraduate
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             As a committed Computer Science undergraduate at the University of
