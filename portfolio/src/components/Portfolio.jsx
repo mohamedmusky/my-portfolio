b72f206e9c6704bd39188b5d3cec7f863a054e6d
musky <mohamedmusky123@gmail.com>
2024-05-24 18:02:23 +0530

    changes

--- portfolio/src/components/Portfolio.jsx
+++ portfolio/src/components/Portfolio.jsx
@@ -5,6 +5,7 @@ import p1 from '../assets/p1.png'
 import p2 from '../assets/p2.png'
 import p3 from '../assets/p3.png'
 import p4 from '../assets/p4.jpg'
+import p5 from '../assets/p5.png'
 /*
 
 
@@ -45,6 +46,14 @@ const Portfolio = () => {
             title : "Goal Add mobile app",
             about : "Technology Used :ReactNative,Expo Go ",
             github : "https://github.com/mohamedmusky/Goal-add-mobile-app",
+            demo : "/"
+        },
+        {
+            id : 5,
+            Image : p5,
+            title : "Portfolio Website",
+            about : "Technology Used : Reactjs,Tailwind CSS",
+            github : "https://github.com/mohamedmusky/my-portfolio/tree/main/portfolio",
             demo : "/"
         },
  
